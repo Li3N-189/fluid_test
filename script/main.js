ef8bd7fade6c6d402ea17ab6781134c3a4cde8d9
Li3N-189 <kym.jrk815@gmail.com>
2026-04-19 13:07:17 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -12,9 +12,9 @@ const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
 const loader = new THREE.TextureLoader();
 
-const storageTextures = Array(6).fill().map(() => {
+const storageTextures = Array(3).fill().map(() => {
   const storageTexture = new StorageTexture(width, height);
-  storageTexture.type = THREE.FloatType;
+  storageTexture.type = THREE.HalfFloatType;
   return storageTexture;
 });
 
@@ -22,20 +22,20 @@ const uTime = uniform(0);
 const uScreenSize = uniform(uvec2(width, height));
 const uMouse = uniform(vec2(0, 0));
 const uDeltaV = uniform(vec2(0, 0));
-const uRadius = uniform(15);
+const uRadius = uniform(10);
 const uDeltaT = uniform(1);
-const uDsp = uniform(0.95);
+const uDsp = uniform(1);
 const uIsDragging = uniform(false);
 
 const addForceNode = addForce(storageTextures[0], storageTextures[1], uScreenSize, uMouse, uDeltaV, uRadius, uIsDragging).compute(width * height);
-const advectVelocityNode = advectVelocity(storageTextures[1], storageTextures[2], uScreenSize, uDeltaT, uDsp).compute(width * height);
-const updateDivergenceNode = updateDivergence(storageTextures[2], storageTextures[3], uScreenSize).compute(width * height);
-const updatePressureNodeA = updatePressure(storageTextures[3], storageTextures[4], uScreenSize).compute(width * height);
-const updatePressureNodeB = updatePressure(storageTextures[4], storageTextures[3], uScreenSize).compute(width * height);
-const subtractGradientNode = subtractGradient(storageTextures[4], storageTextures[0], uScreenSize).compute(width * height);
-const renderNode = renderShader(storageTextures[0], storageTextures[5], uScreenSize, uTime).compute(width * height);
+const advectVelocityNode = advectVelocity(storageTextures[1], storageTextures[0], uScreenSize, uDeltaT, uDsp).compute(width * height);
+const updateDivergenceNode = updateDivergence(storageTextures[0], storageTextures[1], uScreenSize).compute(width * height);
+const updatePressureNodeA = updatePressure(storageTextures[1], storageTextures[0], uScreenSize).compute(width * height);
+const updatePressureNodeB = updatePressure(storageTextures[0], storageTextures[1], uScreenSize).compute(width * height);
+const subtractGradientNode = subtractGradient(storageTextures[1], storageTextures[0], uScreenSize).compute(width * height);
+const renderNode = renderShader(storageTextures[0], storageTextures[2], uScreenSize, uTime).compute(width * height);
 
-const material = new MeshBasicNodeMaterial({map: storageTextures[5]});
+const material = new MeshBasicNodeMaterial({map: storageTextures[2]});
 
 const quad = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material);
 scene.add(quad);
@@ -53,8 +53,8 @@ function animate(tm) {
   uIsDragging.value = pointerManager.isDragging;
   uMouse.value.x = pointerManager.x;
   uMouse.value.y = pointerManager.y;
-  uDeltaV.value.x = pointerManager.deltaX * 500;
-  uDeltaV.value.y = pointerManager.deltaY * 500;
+  uDeltaV.value.x = pointerManager.deltaX * 300;
+  uDeltaV.value.y = pointerManager.deltaY * 300;
   const deltaT = tm - prevTime;
   
   uTime.value = performance.now();
@@ -67,7 +67,7 @@ function animate(tm) {
   for (let i = 0; i < stepCount; i++) {
     renderer.compute(advectVelocityNode);
     renderer.compute(updateDivergenceNode);
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 4; j++) {
       if (j % 2 == 0) renderer.compute(updatePressureNodeA);
       else renderer.compute(updatePressureNodeB);
     }
